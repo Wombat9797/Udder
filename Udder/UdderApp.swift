cc7c100636adc391f2fc531fe2eebd2d3af9352d
Wombat9797 <wombat9797@gmail.com>
2024-10-14 12:50:44 +0900

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Wombat on 2024/10/14.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UdderTests/UdderTests.swift
@@ -0,0 +1,17 @@
+//
+//  UdderTests.swift
+//  UdderTests
+//
+//  Created by Wombat on 2024/10/14.
+//
+
+import Testing
+@testable import Udder
+
+struct UdderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
